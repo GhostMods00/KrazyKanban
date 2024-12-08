d642c9e113180c910b05b5ab68a6763eb94e8f5a
Waleed Zaryab <westendcrew1982@gmail.com>
2024-12-08 18:34:03 +1100

    Added code for auth in utils

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,55 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
 
+interface CustomJwtPayload extends JwtPayload {
+  username: string;
+}
+
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    const token = this.getToken();
+    if (token) {
+      try {
+        return jwtDecode<CustomJwtPayload>(token);
+      } catch (error) {
+        return null;
+      }
+    }
+    return null;
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded.exp) {
+        return decoded.exp < Date.now() / 1000;
+      }
+      return false;
+    } catch (err) {
+      return true;
+    }
   }
 
   getToken(): string {
-    // TODO: return the token
+    return localStorage.getItem('id_token') || '';
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    // Save token to localStorage
+    localStorage.setItem('id_token', idToken);
+    // Redirect to home page
+    window.location.assign('/');
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    // Remove token from localStorage
+    localStorage.removeItem('id_token');
+    // Redirect to login page
+    window.location.assign('/login');
   }
 }
 
